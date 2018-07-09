dfe4bc990c3413039496643d97c4f614d5e00832
Hari Charan U <31123971+wolfharan@users.noreply.github.com>
2018-07-09 22:22:04 +0530

    Update client.py

--- client.py
+++ client.py
@@ -5,7 +5,6 @@ import numpy as np
 
 data = np.genfromtxt('train.csv', delimiter=',',dtype=np.float64)
 weights=np.array([1,1,1,1,1,1,1,1,1,1,1,1,1])
-N=len(data)-1
 weights=grad_run(weights,data,0.0000001)
 print(weights)
 print(meansqrerror(weights,data))
