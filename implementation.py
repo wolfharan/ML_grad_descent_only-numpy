40162ab9f9c90e885a86db017759e400fcf25975
Hari Charan U <31123971+wolfharan@users.noreply.github.com>
2018-07-09 22:58:47 +0530

    Update implementation.py

--- implementation.py
+++ implementation.py
@@ -21,9 +21,9 @@ def gradient(weights,data,learnrate):
     nweights = weights-(learnrate*w_grad)
     return nweights
 
-def grad_run(weights,data,learnrate):
+def grad_run(weights,data,learnrate,iterations):
     mse=meansqrerror(weights,data)
-    for i in range(800000):
+    for i in range(iterations):
         weights=gradient(weights,data,learnrate)
         mse=meansqrerror(weights,data)
     return weights
